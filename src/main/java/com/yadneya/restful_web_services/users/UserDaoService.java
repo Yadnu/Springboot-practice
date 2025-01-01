18bea0e4a8ba000000ef41f1f883ed353c8a3c33
yadnu <yadneyajoshi3@gmail.com>
2025-01-01 01:51:32 -0800

    practicing rest

---
+++ src/main/java/com/yadneya/restful_web_services/users/UserDaoService.java
@@ -0,0 +1,13 @@
+package com.yadneya.restful_web_services.users;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+	// public List<User> findAll() {
+	
+	// public User save(User user) {
+	
+	//public User findOne ( int id) {
+	
+}
